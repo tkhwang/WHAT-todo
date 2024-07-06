38799e34f8a42c35523100fb7a20142b59ee5c69
tkhwang <tkhwang@gmail.com>
2024-07-06 17:33:53 +0900

    ✨ feat(mobile-app): check interwork with packages/ui

--- apps/mobile-app/app/index.tsx
+++ apps/mobile-app/app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { Text, View } from "react-native"
+import { greetingFromUi } from "@expertTodo/ui"
 
 export default function Index() {
   return (
@@ -9,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>{greetingFromUi}</Text>
     </View>
-  );
+  )
 }
