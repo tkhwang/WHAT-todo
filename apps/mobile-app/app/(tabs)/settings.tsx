aed5855c4ac3d98803ae313f19939f5823790780
tkhwang <tkhwang@gmail.com>
2024-07-12 21:57:35 +0900

    ✨ feat(layout): implement tab menu

---
+++ apps/mobile-app/app/(tabs)/settings.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function Tab() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Tab [Home|Settings]</Text>
+    </View>
+  )
+}
