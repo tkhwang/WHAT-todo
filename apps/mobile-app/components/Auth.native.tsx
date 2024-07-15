e56dfbad6c31a4411f87ea6b5d3f01c501a3c8ec
tkhwang <tkhwang@gmail.com>
2024-07-15 21:50:54 +0900

    ✨ feat(mobile): add <Auth /> component

---
+++ apps/mobile-app/components/Auth.native.tsx
@@ -0,0 +1,54 @@
+import { Platform } from "react-native"
+import * as AppleAuthentication from "expo-apple-authentication"
+import { supabase } from "../utils/supabase"
+
+import "react-native-url-polyfill/auto"
+import { createClient } from "@supabase/supabase-js"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+export function Auth() {
+  if (Platform.OS === "ios")
+    return (
+      <AppleAuthentication.AppleAuthenticationButton
+        buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
+        buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
+        cornerRadius={5}
+        style={{ width: 200, height: 64 }}
+        onPress={async () => {
+          try {
+            const credential = await AppleAuthentication.signInAsync({
+              requestedScopes: [
+                AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+                AppleAuthentication.AppleAuthenticationScope.EMAIL,
+              ],
+            })
+            console.log("🚀 ~ onPress={ ~ credential:", credential)
+
+            // Sign in via Supabase Auth.
+            if (credential.identityToken) {
+              const {
+                error,
+                data: { user },
+              } = await supabase.auth.signInWithIdToken({
+                provider: "apple",
+                token: credential.identityToken,
+              })
+              console.log(JSON.stringify({ error, user }, null, 2))
+              if (!error) {
+                // User is signed in.
+              }
+            } else {
+              throw new Error("No identityToken.")
+            }
+          } catch (e) {
+            if (e.code === "ERR_REQUEST_CANCELED") {
+              // handle that the user canceled the sign-in flow
+            } else {
+              // handle other errors
+            }
+          }
+        }}
+      />
+    )
+  return <>{/* Implement Android Auth options. */}</>
+}
