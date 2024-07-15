13976a2d5cb9b6495bc2693688b0908a48caa480
tkhwang <tkhwang@gmail.com>
2024-07-15 21:51:22 +0900

    ✨ feat(mobile): add <Auth /> in index

--- apps/mobile-app/app/(tabs)/index.tsx
+++ apps/mobile-app/app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
+import { Auth } from "@/components/Auth"
 import { View, Text } from "react-native"
 
 export default function HomeScreen() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-4xl font-bold">Home</Text>
+      <Auth />
     </View>
   )
 }
