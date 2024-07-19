e833895a752b15f8a81718b2efda5b89599ad5cf
tkhwang <tkhwang@gmail.com>
2024-07-20 05:38:23 +0900

    ✨ feat(mobile/ui): add gluestack  HStack/VStack

--- apps/mobile-app/app/(public)/login.tsx
+++ apps/mobile-app/app/(public)/login.tsx
@@ -1,9 +1,11 @@
 import { Auth } from "@/components/Auth";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { Text } from "@/components/ui/text";
 
 export default function login() {
   return (
     <View className="flex-1 justify-center items-center">
+      <Text className="text-2xl font-bold text-black mb-4">Welcome back to WhatTodo</Text>
       <Auth />
     </View>
   );
