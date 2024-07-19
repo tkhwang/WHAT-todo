04108bc8e9fd37fe8c192a9fcc709307f9ecd553
tkhwang <tkhwang@gmail.com>
2024-07-20 05:46:15 +0900

    ✨ feat(mobile/tui): use <Heading /> gluestack component

--- apps/mobile-app/app/(public)/login.tsx
+++ apps/mobile-app/app/(public)/login.tsx
@@ -1,12 +1,11 @@
 import { Auth } from "@/components/Auth";
-import { View } from "react-native";
-import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
+import { Heading } from "@/components/ui/heading";
 
 export default function login() {
   return (
     <VStack className="flex-1 justify-center items-center" space="md">
-      <Text className="text-2xl font-bold text-black">Welcome back to WhatTodo</Text>
+      <Heading>Welcome back to whatTodo</Heading>
       <Auth />
     </VStack>
   );
