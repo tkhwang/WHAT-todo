640d3129641b4835fa03a2180df656cc56406f91
tkhwang <tkhwang@gmail.com>
2024-07-20 05:19:54 +0900

    ✨ feat(mobile): setup gluestack-ui configuration

--- apps/mobile-app/app/_layout.tsx
+++ apps/mobile-app/app/_layout.tsx
@@ -1,4 +1,6 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
@@ -39,20 +41,20 @@ export default function RootLayout() {
 
   if (!loaded) return null;
 
-  return <RootLayoutNav />;
+  return <GluestackUIProvider mode="light"><RootLayoutNav /></GluestackUIProvider>;
 }
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <AuthProvider>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(auth)/(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="(public)/login" options={{ headerShown: false }} />
-        </Stack>
-      </ThemeProvider>
-    </AuthProvider>
+    <GluestackUIProvider mode="light"><AuthProvider>
+        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+          <Stack>
+            <Stack.Screen name="(auth)/(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="(public)/login" options={{ headerShown: false }} />
+          </Stack>
+        </ThemeProvider>
+      </AuthProvider></GluestackUIProvider>
   );
 }
--- apps/mobile-app/babel.config.js
+++ apps/mobile-app/babel.config.js
@@ -1,7 +1,15 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel"],
-  }
+    presets: [["babel-preset-expo", {
+      jsxImportSource: "nativewind"
+    }], "nativewind/babel"],
+    plugins: [["module-resolver", {
+      root: ["./"],
+
+      alias: {
+        "@": "./"
+      }
+    }]],
+  };
 }
