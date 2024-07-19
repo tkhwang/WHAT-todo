181d29c44423c5f21a4b9ec9b00efa9d907d8537
tkhwang <tkhwang@gmail.com>
2024-07-20 05:42:39 +0900

    ✨ feat(mobile/ui): use gluestack component in login screen

--- apps/mobile-app/app/(public)/login.tsx
+++ apps/mobile-app/app/(public)/login.tsx
@@ -1,12 +1,13 @@
 import { Auth } from "@/components/Auth";
 import { View } from "react-native";
 import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
 
 export default function login() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-2xl font-bold text-black mb-4">Welcome back to WhatTodo</Text>
+    <VStack className="flex-1 justify-center items-center" space="md">
+      <Text className="text-2xl font-bold text-black">Welcome back to WhatTodo</Text>
       <Auth />
-    </View>
+    </VStack>
   );
 }
