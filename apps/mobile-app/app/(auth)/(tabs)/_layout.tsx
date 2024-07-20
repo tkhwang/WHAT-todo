36756e9bb6b39fe06c87a61422251eab7a3c5937
tkhwang <tkhwang@gmail.com>
2024-07-21 06:37:20 +0900

    ✨ feat(mobile/ui): use i18n in mobile/ui

--- apps/mobile-app/app/(auth)/(tabs)/_layout.tsx
+++ apps/mobile-app/app/(auth)/(tabs)/_layout.tsx
@@ -1,65 +1,69 @@
-import React from "react"
-import FontAwesome from "@expo/vector-icons/FontAwesome"
-import { Link, Tabs } from "expo-router"
-import { Pressable } from "react-native"
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import { Pressable } from "react-native";
 
-import { TabBarIcon } from "@/components/navigation/TabBarIcon"
-import { Colors } from "@/constants/Colors"
-import { useColorScheme } from "@/hooks/useColorScheme"
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { useTranslation } from "react-i18next";
 
 // function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>["name"]; color: string }) {
 //   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 // }
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme()
+  const { t } = useTranslation();
+  const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
+        headerShown: false
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
-          tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? "home" : "home-outline"} color={color} />,
+          title: t("app.menu.home"),
+          tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? "home" : "home-outline"} color={color} />
         }}
       />
       <Tabs.Screen
         name="me"
         options={{
-          title: "Me",
+          title: t("app.menu.me"),
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? "checkmark-circle-sharp" : "checkmark-circle-outline"} color={color} />
-          ),
+          )
         }}
       />
       <Tabs.Screen
         name="expert"
         options={{
-          title: "Expert",
+          title: t("app.menu.expert"),
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? "chatbubbles-sharp" : "chatbubbles-outline"} color={color} />
-          ),
+          )
         }}
       />
       <Tabs.Screen
         name="supervisor"
         options={{
-          title: "Supervisor",
-          tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? "eye" : "eye-outline"} color={color} />,
+          title: t("app.menu.supervisor"),
+          tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? "eye" : "eye-outline"} color={color} />
         }}
       />
       <Tabs.Screen
-        name="setting"
+        name="profile"
         options={{
-          title: "Setting",
-          tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? "cog" : "cog-outline"} color={color} />,
+          title: t("app.menu.profile"),
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? "id-card" : "id-card-outline"} color={color} />
+          )
         }}
       />
     </Tabs>
-  )
+  );
 }
--- apps/mobile-app/app/(public)/login.tsx
+++ apps/mobile-app/app/(public)/login.tsx
@@ -1,12 +1,15 @@
-import { Auth } from "@/components/Auth/Auth"
-import AppleLogin from "@/components/Auth/AppleLogin.ios"
-import { View, Text } from "react-native"
+import { Auth } from "@/components/Auth/Auth";
+import AppleLogin from "@/components/Auth/AppleLogin.ios";
+import { View, Text } from "react-native";
+import { useTranslation } from "react-i18next";
 
 export default function login() {
+  const { t } = useTranslation();
+
   return (
     <View className="flex-1 justify-center items-center">
       <Auth />
       <AppleLogin />
     </View>
-  )
+  );
 }
