e3be51d837cc532718c7306877ac521866f83953
tkhwang <tkhwang@gmail.com>
2024-07-21 00:09:23 +0900

    ✨ feat(mobile/auth): setup supabase login after apple login

---
+++ apps/mobile-app/lib/supabase.ts
@@ -0,0 +1,17 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { createClient } from "@supabase/supabase-js"
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
+
+if (!supabaseUrl) throw new Error("EXPO_PUBLIC_SUPABASE_URL is not set.")
+if (!supabaseAnonKey) throw new Error("EXPO_PUBLIC_SUPABASE_ANON_KEY is not set.")
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
