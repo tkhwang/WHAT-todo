301364d3eff3f104e3b155ecbfabcb4fe6ed5b12
tkhwang <tkhwang@gmail.com>
2024-07-21 06:36:31 +0900

    ✨ feat(mobile/i18n): setup i18n configuration

---
+++ apps/mobile-app/utils/i18n.ts
@@ -0,0 +1,33 @@
+import i18n, { use } from "i18next";
+import { initReactI18next } from "react-i18next";
+import * as Localization from "expo-localization";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import ko from "@/constants/locale/ko.json";
+import en from "@/constants/locale/en.json";
+
+const initI18n = async () => {
+  let savedLanguage = await AsyncStorage.getItem("language");
+
+  if (!savedLanguage) {
+    savedLanguage = Localization.getLocales()[1].languageCode;
+  }
+
+  i18n.use(initReactI18next).init({
+    compatibilityJSON: "v3",
+    debug: true,
+    lng: savedLanguage ?? "ko",
+    fallbackLng: "ko",
+    interpolation: {
+      escapeValue: false
+    },
+    resources: {
+      ko: { translation: ko },
+      en: { translation: en }
+    }
+  });
+};
+
+initI18n();
+
+export default i18n;
