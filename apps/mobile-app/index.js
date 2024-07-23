46dc9beef34f8037cb50d2e1b2f8f12a61e29dfc
tkhwang <tkhwang@gmail.com>
2024-07-23 21:26:49 +0900

    ✨ feat(mobile/expo): setup expo-dev-tool

--- apps/mobile-app/index.js
+++ apps/mobile-app/index.js
@@ -1,8 +1,9 @@
-import { registerRootComponent } from "expo"
+import "expo-dev-client";
+import { registerRootComponent } from "expo";
 
-import Index from "./App"
+import Index from "./App";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(Index)
+registerRootComponent(Index);
