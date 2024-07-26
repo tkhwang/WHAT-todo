f8b6e149182cb33b0819969b92c6d5bbd04cec5d
tkhwang <tkhwang@gmail.com>
2024-07-27 01:03:06 +0900

    ✨ feat(mobile/ui): setup react-native-reusables

--- apps/mobile-app/app/_layout.tsx
+++ apps/mobile-app/app/_layout.tsx
@@ -1,7 +1,7 @@
 import "../global.css";
 
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { DarkTheme, DefaultTheme, Theme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
@@ -9,9 +9,20 @@ import React, { useEffect } from "react";
 
 import "@/utils/i18n";
 import AuthProvider from "@/context/AuthProvider";
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryClientProvider } from "@/config/ReactQueryClientProvider";
+import { NAV_THEME } from "@/constants/uiConsts";
+import { useColorScheme } from "@/lib/useColorScheme";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Platform } from "react-native";
+
+const LIGHT_THEME: Theme = {
+  dark: false,
+  colors: NAV_THEME.light
+};
+const DARK_THEME: Theme = {
+  dark: true,
+  colors: NAV_THEME.dark
+};
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -48,13 +59,42 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const queryClient = new QueryClient();
-  const colorScheme = useColorScheme();
+  const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    (async () => {
+      const theme = await AsyncStorage.getItem("theme");
+      if (Platform.OS === "web") {
+        // Adds the background color to the html element to prevent white background on overscroll.
+        document.documentElement.classList.add("bg-background");
+      }
+      if (!theme) {
+        AsyncStorage.setItem("theme", colorScheme);
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      const colorTheme = theme === "dark" ? "dark" : "light";
+      if (colorTheme !== colorScheme) {
+        setColorScheme(colorTheme);
+
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      setIsColorSchemeLoaded(true);
+    })().finally(() => {
+      SplashScreen.hideAsync();
+    });
+  }, []);
+
+  if (!isColorSchemeLoaded) {
+    return null;
+  }
 
   return (
     <ReactQueryClientProvider>
       <AuthProvider>
-        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
           <Stack>
             <Stack.Screen name="(auth)/(tabs)" options={{ headerShown: false }} />
             <Stack.Screen name="(public)/login" options={{ headerShown: false }} />
