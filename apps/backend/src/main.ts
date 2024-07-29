ecdb6b0b2b39e601709f75db559466fa42ce3741
tkhwang <tkhwang@gmail.com>
2024-07-29 22:31:41 +0900

    🐳 chore(backend/debug): add console.log to print port number

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(parseInt(process.env.PORT) || 3000);
+  const port = parseInt(process.env.PORT) || 3000;
+  await app.listen(port);
+  console.log(`[+][bootstrap] Server is running on port ${port}`);
 }
 bootstrap();
