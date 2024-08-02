972c98cbc0642fe150e3d41cfaf10bf663d87b42
tkhwang <tkhwang@gmail.com>
2024-08-03 07:25:56 +0900

    ✨ feat(backend/pipe): enable auto transform in globalPipes

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = parseInt(process.env.PORT) || 3000;
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   await app.listen(port);
   console.log(`[+][bootstrap] Server is running on port ${port}`);
