fe3a93ea92e4f122a1036e2bee2b8b9dd00ece26
tkhwang <tkhwang@gmail.com>
2024-08-03 07:08:43 +0900

    ✨ feat(backend/common): add global pipes

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = parseInt(process.env.PORT) || 3000;
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(port);
   console.log(`[+][bootstrap] Server is running on port ${port}`);
 }
