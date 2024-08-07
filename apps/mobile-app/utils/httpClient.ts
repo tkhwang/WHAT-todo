f3c2ed0b53dbb0739c40d1b0d1ca57b5c4ae131a
tkhwang <tkhwang@gmail.com>
2024-08-07 21:03:32 +0900

    ✨ feat(mobile/network): add httpClient

---
+++ apps/mobile-app/utils/httpClient.ts
@@ -0,0 +1,32 @@
+import { AxiosRequestConfigWithMetadata } from "@whatTodo/models";
+import axios, { AxiosResponse, InternalAxiosRequestConfig } from "axios";
+
+if (process.env.EXPO_PUBLIC_IS_PRODUCTION) throw new Error("EXPO_PUBLIC_IS_PRODUCTION is not set");
+if (process.env.EXPO_PUBLIC_API_URL) throw new Error("EXPO_PUBLIC_API_URL is not set");
+
+export const httpClient = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_API_URL
+});
+
+function interceptRequest(config: AxiosRequestConfigWithMetadata) {
+  if (process.env.EXPO_PUBLIC_IS_PRODUCTION) {
+    config.metadata = { startTime: new Date() };
+  }
+  return config;
+}
+function interceptErrorRequest(error: any) {
+  return Promise.reject(error);
+}
+
+function interceptResponse(response: AxiosResponse) {
+  if (process.env.EXPO_PUBLIC_IS_PRODUCTION) {
+    response.config.metadata.endTime = new Date();
+  }
+  return response;
+}
+function interceptErrorResponse(error: any) {
+  return Promise.reject(error);
+}
+
+httpClient.interceptors.request.use(interceptRequest, interceptErrorRequest);
+httpClient.interceptors.response.use(interceptResponse, interceptErrorResponse);
