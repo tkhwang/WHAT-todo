c985befca10336d1cb5f50795b49da179f30c07d
tkhwang <tkhwang@gmail.com>
2024-08-13 05:38:19 +0900

    ✨ feat(mobile/icon): add <Tag /> icon

--- apps/mobile-app/assets/icons/index.tsx
+++ apps/mobile-app/assets/icons/index.tsx
@@ -1,9 +1,10 @@
-import { SvgProps } from "react-native-svg";
+import { NumberProp, SvgProps } from "react-native-svg";
 
 import InboxCheck from "./InboxCheck";
 import Home from "./Home";
 import ArrowLeft from "./ArrowLeft";
 import AccountSetting from "./AccountSetting";
+import Tag from "./Tag";
 
 import { appTheme } from "@/constants/uiConsts";
 
@@ -12,24 +13,27 @@ const icons = {
   inboxCheck: InboxCheck,
   arrowLeft: ArrowLeft,
   accountSetting: AccountSetting,
+  tag: Tag,
 };
 
 type IconNames = keyof typeof icons;
 
 interface Props extends SvgProps {
   name: IconNames;
-  size?: number;
+  size: NumberProp;
+  strokeWidth: number;
 }
 
-export default function Icon({ name, ...props }: Props) {
+export default function Icon({ size, name, strokeWidth, ...props }: Props) {
   const IconComponent = icons[name];
 
   return (
     <IconComponent
-      height={props.size ?? 24}
-      width={props.size ?? 24}
-      strokeWidth={props.strokeWidth ?? 1.9}
+      height={size ?? 24}
+      width={size ?? 24}
+      strokeWidth={strokeWidth ?? 1.9}
       color={appTheme.colors.textLight}
+      {...props}
     />
   );
 }
