0c12399bcbfc0f539f1c8d986d28a726ee1e3b7e
tkhwang <tkhwang@gmail.com>
2024-08-12 20:08:12 +0900

    🌈 style(mobile/icon): add icons

---
+++ apps/mobile-app/assets/icons/ArrowLeft.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import Svg, { SvgProps, Path } from "react-native-svg";
+
+interface Props extends SvgProps {
+  strokeWidth: number;
+}
+
+export default function ArrowLeft({ strokeWidth, ...props }: Props) {
+  return (
+    <Svg
+      xmlns={"http://www.w3.org/2000/svg"}
+      viewBox={"0 0 24 24"}
+      width={24}
+      height={24}
+      color={"#000000"}
+      fill={"none"}
+      {...props}
+    >
+      <Path
+        d={"M15 6C15 6 9.00001 10.4189 9 12C8.99999 13.5812 15 18 15 18"}
+        stroke={"currentColor"}
+        strokeWidth={strokeWidth}
+        strokeLinecap={"round"}
+        strokeLinejoin={"round"}
+      />
+    </Svg>
+  );
+}
---
+++ apps/mobile-app/assets/icons/index.tsx
@@ -0,0 +1,33 @@
+import { SvgProps } from "react-native-svg";
+
+import InboxCheck from "./InboxCheck";
+import Home from "./Home";
+import ArrowLeft from "./ArrowLeft";
+
+import { appTheme } from "@/constants/uiConsts";
+
+const icons = {
+  home: Home,
+  inboxCheck: InboxCheck,
+  arrowLeft: ArrowLeft,
+};
+
+type IconNames = keyof typeof icons;
+
+interface Props extends SvgProps {
+  name: IconNames;
+  size?: number;
+}
+
+export default function Icon({ name, ...props }: Props) {
+  const IconComponent = icons[name];
+
+  return (
+    <IconComponent
+      height={props.size ?? 24}
+      width={props.size ?? 24}
+      strokeWidth={props.strokeWidth ?? 1.9}
+      color={appTheme.colors.textLight}
+    />
+  );
+}
