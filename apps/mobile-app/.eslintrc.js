a3331b074849a2f88f0c23810b5eb3628daec807
tkhwang <tkhwang@gmail.com>
2024-08-14 06:09:11 +0900

    ✨ feat(eslint): configure eslint to recognize typescript path

--- apps/mobile-app/.eslintrc.js
+++ apps/mobile-app/.eslintrc.js
@@ -57,8 +57,11 @@ module.exports = {
   },
   settings: {
     "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
       typescript: {
-        // project: "./apps/mobile-app/tsconfig.json",
+        project: "./apps/mobile-app/tsconfig.json",
       },
     },
   },
