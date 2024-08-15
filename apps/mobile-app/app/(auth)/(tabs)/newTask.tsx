1726362118f8d7492452cc10d5f315b266e8aaa0
tkhwang <tkhwang@gmail.com>
2024-08-15 19:25:57 +0900

    ✨ feat(mobile/ui): implement buttomSheet using reanimated
    reference : https://docs.swmansion.com/react-native-reanimated/examples/bottomsheet/

--- apps/mobile-app/app/(auth)/(tabs)/newTask.tsx
+++ apps/mobile-app/app/(auth)/(tabs)/newTask.tsx
@@ -1,42 +1,89 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Animated, Pressable, StyleSheet, Text, View } from "react-native";
 import React, { useCallback, useRef } from "react";
 import { useTranslation } from "react-i18next";
-import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { useSharedValue } from "react-native-reanimated";
 
 import ScreenWrapper from "@/components/MainLayout/ScreenWrapper";
 import MainHeader from "@/components/MainLayout/MainHeader";
+import { useColorScheme } from "@/lib/useColorScheme";
+import { BottomSheet } from "@/components/BottomSheet";
 
 export default function NewTaskScreen() {
-  const { t } = useTranslation();
+  const { colorScheme } = useColorScheme();
+  const isOpen = useSharedValue(false);
 
-  const bottomSheetRef = useRef<BottomSheet>(null);
+  const toggleSheet = () => {
+    isOpen.value = !isOpen.value;
+  };
 
-  const handleSheetChanges = useCallback((index: number) => {
-    console.log("handleSheetChanges", index);
-  }, []);
+  const contentStyle = {
+    color: colorScheme === "light" ? "#001a72" : "#f8f9ff",
+    textDecorationColor: colorScheme === "light" ? "#001a72" : "#f8f9ff",
+  };
 
   return (
     <ScreenWrapper>
       <MainHeader />
-      <View style={styles.container}>
-        <BottomSheet ref={bottomSheetRef} snapPoints={["50%"]} onChange={handleSheetChanges}>
-          <BottomSheetView style={styles.contentContainer}>
-            <Text className={"text-base font-medium"}>{t("task.create.title")}</Text>
-          </BottomSheetView>
-        </BottomSheet>
+      <View style={styles.safeArea}>
+        <View style={styles.flex} />
+        <Pressable style={styles.toggleButton} onPress={toggleSheet}>
+          <Text style={styles.toggleButtonText}>{"Toggle bottom sheet"}</Text>
+        </Pressable>
+        <View style={styles.flex} />
       </View>
+      <BottomSheet isOpen={isOpen} toggleSheet={toggleSheet}>
+        <Animated.Text style={contentStyle}>
+          {"Discover the indispensable convenience of a bottom sheet in mobile app. Seamlessly integrated, it provides"}
+          {"quick access to supplementary features and refined details."}
+        </Animated.Text>
+        <View style={styles.buttonContainer}>
+          <Pressable style={[styles.bottomSheetButton]}>
+            <Text style={[styles.bottomSheetButtonText, contentStyle]}>{"Read more"}</Text>
+          </Pressable>
+        </View>
+      </BottomSheet>
     </ScreenWrapper>
   );
 }
 
 const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    padding: 24,
-    backgroundColor: "grey",
+    height: 250,
   },
-  contentContainer: {
-    flex: 1,
+  buttonContainer: {
+    marginTop: 16,
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-around",
+  },
+  toggleButton: {
+    backgroundColor: "#b58df1",
+    padding: 12,
+    borderRadius: 48,
+  },
+  toggleButtonText: {
+    color: "white",
+    padding: "0.5rem",
+  },
+  safeArea: {
     alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+  bottomSheetButton: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+    paddingBottom: 2,
+  },
+  bottomSheetButtonText: {
+    fontWeight: 600,
+    textDecorationLine: "underline",
   },
 });
