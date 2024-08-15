1626636488bc0635cc2606c16f514a8be33a877a
tkhwang <tkhwang@gmail.com>
2024-08-15 15:17:47 +0900

    ✨ feat(mobile/screen): add <NewTask /> screen

--- apps/mobile-app/app/(auth)/(tabs)/newTask.tsx
+++ apps/mobile-app/app/(auth)/(tabs)/newTask.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import { useTranslation } from "react-i18next";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 
 import ScreenWrapper from "@/components/MainLayout/ScreenWrapper";
 import MainHeader from "@/components/MainLayout/MainHeader";
@@ -8,14 +9,35 @@ import MainHeader from "@/components/MainLayout/MainHeader";
 export default function NewTaskScreen() {
   const { t } = useTranslation();
 
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+
   return (
     <ScreenWrapper>
       <MainHeader />
-      <View className={"flex-1 justify-center items-center"}>
-        <Text className={"text-xl font-semibold"}>{t("app.screen.newTask")}</Text>
+      <View style={styles.container}>
+        <BottomSheet snapPoints={["50%"]} ref={bottomSheetRef} onChange={handleSheetChanges}>
+          <BottomSheetView style={styles.contentContainer}>
+            <Text>{"Awesome 🎉"}</Text>
+          </BottomSheetView>
+        </BottomSheet>
       </View>
     </ScreenWrapper>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "grey",
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+});
