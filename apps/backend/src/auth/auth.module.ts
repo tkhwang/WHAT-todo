db51ff003b61bafea94b6c67c12bfe6239200d2c
tkhwang <tkhwang@gmail.com>
2024-08-17 16:51:57 +0900

    ✨ feat(backend/todo): add AuthGuard to /todos/addTodo router

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { FirebaseAuthStrategy } from './firebase-auth.strategy';
 
 @Module({
   imports: [UsersModule],
   controllers: [AuthController],
+  providers: [FirebaseAuthStrategy],
 })
 export class AuthModule {}
--- apps/backend/src/todos/todos.controller.ts
+++ apps/backend/src/todos/todos.controller.ts
@@ -1,12 +1,14 @@
 import { AddTodoRequest } from '@whatTodo/models';
 import { TodosService } from './todos.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('todos')
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}
 
   @Post('addTodo')
+  @UseGuards(AuthGuard('bearer'))
   async addTodo(@Body() newTodoDto: AddTodoRequest) {
     return this.todosService.addTodo(newTodoDto);
   }
