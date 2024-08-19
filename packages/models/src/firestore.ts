86913d1f5715f24cc6722eed5132a75bab4ff27f
tkhwang <tkhwang@gmail.com>
2024-08-20 07:19:19 +0900

    ✨ feat(packages/models): add firebase library

--- packages/models/src/firestore.ts
+++ packages/models/src/firestore.ts
@@ -1,4 +1,8 @@
+import { firebase } from "@react-native-firebase/firestore"
+
 export const COLLECTIONS = {
   USERS: "users",
   TODOS: "todos",
 }
+
+export class FirestoreTimestamp extends firebase.firestore.Timestamp {}
