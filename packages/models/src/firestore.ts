4f4cc2911a6421cb4c06e30aaaeb2381e945a8d7
tkhwang <tkhwang@gmail.com>
2024-08-20 08:31:03 +0900

    🐞 fix(models/firestore): use firebase-admin which is used in backend

--- packages/models/src/firestore.ts
+++ packages/models/src/firestore.ts
@@ -1,8 +1,8 @@
-import { firebase } from "@react-native-firebase/firestore"
+import { firestore } from "firebase-admin"
 
 export const COLLECTIONS = {
   USERS: "users",
   TODOS: "todos",
 }
 
-export class FirestoreTimestamp extends firebase.firestore.Timestamp {}
+export class FirestoreTimestamp extends firestore.Timestamp {}
