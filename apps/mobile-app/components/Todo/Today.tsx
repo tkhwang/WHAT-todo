9b7d6d0358dea5fb6717a76c98a6179663999956
tkhwang <tkhwang@gmail.com>
2024-08-20 20:33:05 +0900

    🐳 chore(mobile/todo): add useUserTodos() query result for debugging

--- apps/mobile-app/components/Todo/Today.tsx
+++ apps/mobile-app/components/Todo/Today.tsx
@@ -1,11 +1,19 @@
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 
 import { Text } from "@/components/ui/text";
+import { useUserTodos } from "@/hooks/queries/useUserTodos";
 
 export default function Today() {
+  const { data: userTodos } = useUserTodos();
+
   return (
     <View className={"flex-1 p-4 gap-4"}>
       <Text className={"text-4xl font-bold"}>{"Today"}</Text>
+      {(userTodos ?? []).map((userTodo, index) => (
+        <Text key={userTodo.id} className={"text-xl font-normal"}>
+          {userTodo.id}
+        </Text>
+      ))}
       {/* Overdue */}
       <Text className={"text-2xl font-bold"}>{"Overdue"}</Text>
       <View className={"h-[1] bg-gray-400 mb-4"} />
@@ -16,5 +24,3 @@ export default function Today() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
