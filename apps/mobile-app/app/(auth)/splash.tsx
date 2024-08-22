d9cc21430b52a16cb2c1d236733d905c83c84517
tkhwang <tkhwang@gmail.com>
2024-08-23 06:19:08 +0900

    ✨ feat(mobile/splash): add splash screen

---
+++ apps/mobile-app/app/(auth)/splash.tsx
@@ -0,0 +1,30 @@
+import { Image, View } from "react-native";
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
+
+import ScreenWrapper from "@/components/MainLayout/ScreenWrapper";
+import { Text } from "@/components/ui/text";
+import { EAS_UPDATE_VERSION_SEMVER } from "@/constants/appConsts";
+
+export default function SplashScreen() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace("/(auth)/(tabs)/");
+    }, 500);
+  }, [router]);
+
+  return (
+    <ScreenWrapper>
+      <View className={"flex-1 justify-between items-center mb-6"}>
+        <Image className={"w-60 h-60 mt-auto mb-auto"} source={require("@/assets/images/splash.png")} />
+        <Text className={"text-sm text-gray-500"}>{"WHAT-todo"}</Text>
+        <Text className={"text-sm text-gray-500"}>
+          {"Version: $"}
+          {EAS_UPDATE_VERSION_SEMVER}
+        </Text>
+      </View>
+    </ScreenWrapper>
+  );
+}
