9f466cb9ef012dcc4129669cab8bf17e034e6039
tkhwang <tkhwang@gmail.com>
2024-08-24 15:38:06 +0900

    ✨ feat(mobile/screen): add <Header /> with back icon in todo detail

--- apps/mobile-app/app/(auth)/(tabs)/todos/[id]/index.tsx
+++ apps/mobile-app/app/(auth)/(tabs)/todos/[id]/index.tsx
@@ -1,15 +1,18 @@
 import { useLocalSearchParams } from "expo-router";
+import { useTranslation } from "react-i18next";
 
 import ScreenWrapper from "@/components/MainLayout/ScreenWrapper";
-import MainHeader from "@/components/MainLayout/MainHeader";
 import Todo from "@/components/Todo/Todo";
+import Header from "@/components/MainLayout/Header";
 
 export default function TodoScreen() {
   const { id: todoId } = useLocalSearchParams() as { id: string };
 
+  const { t } = useTranslation();
+
   return (
     <ScreenWrapper>
-      <MainHeader />
+      <Header title={t("screen.todo.title")} showBackButton />
       <Todo todoId={todoId} />
     </ScreenWrapper>
   );
