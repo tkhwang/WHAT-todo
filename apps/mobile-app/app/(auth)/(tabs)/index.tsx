dbc30f12d381bf13c03ebf36a14b5d89e122c7c3
tkhwang <tkhwang@gmail.com>
2024-08-24 22:51:11 +0900

    🐞 fix(mobile/todo): fix to use bottomsheet component, not screen

--- apps/mobile-app/app/(auth)/(tabs)/index.tsx
+++ apps/mobile-app/app/(auth)/(tabs)/index.tsx
@@ -1,14 +1,21 @@
+import { useRef } from "react";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
+
 import ScreenWrapper from "@/components/MainLayout/ScreenWrapper";
 import MainHeader from "@/components/MainLayout/MainHeader";
 import FloatingActionButton from "@/components/Button/FloatingActionButton";
 import Today from "@/components/Today";
+import AddTodoBottomSheetSimple from "@/components/Todo/add/AddTodoBottomSheetSimple";
 
 export default function HomeScreen() {
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
   return (
     <ScreenWrapper>
       <MainHeader />
       <Today />
-      <FloatingActionButton />
+      <FloatingActionButton bottomSheetModalRef={bottomSheetModalRef} />
+      <AddTodoBottomSheetSimple bottomSheetModalRef={bottomSheetModalRef} />
     </ScreenWrapper>
   );
 }
