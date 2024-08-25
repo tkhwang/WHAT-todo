5e07716b4ae83cbfc2a19cb0345ec3bcd27a48a7
tkhwang <tkhwang@gmail.com>
2024-08-25 16:29:37 +0900

    ✨ feat(mobile/addTodo): set due date

--- apps/mobile-app/components/Todo/add/AddDueDateBottomSheet.tsx
+++ apps/mobile-app/components/Todo/add/AddDueDateBottomSheet.tsx
@@ -4,6 +4,7 @@ import { BottomSheetBackdrop, BottomSheetModal } from "@gorhom/bottom-sheet";
 import { BottomSheetModalMethods } from "@gorhom/bottom-sheet/lib/typescript/types";
 import { useTranslation } from "react-i18next";
 import { useRouter } from "expo-router";
+import { days } from "@whatTodo/models";
 
 import Icon from "@/assets/icons";
 import { useDueDateStore } from "@/stores/dueDate";
@@ -13,10 +14,15 @@ interface Props {
   bottomSheetModalRef: RefObject<BottomSheetModalMethods>;
 }
 export default function AddDueDateBottomSheet({ todoId, bottomSheetModalRef }: Props) {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   const router = useRouter();
 
-  const { setToday, setTomorrow, setNextWeek, reset: resetDueDateStore } = useDueDateStore();
+  const chosenLanguage = useMemo<"en" | "ko">(() => i18n.language, [i18n.language]);
+
+  const todayDay = useDueDateStore((state) => state.todayDay);
+  const tomorrowDay = useDueDateStore((state) => state.tomorrowDay);
+  const reset = useDueDateStore((state) => state.reset);
+  const setToday = useDueDateStore((state) => state.setToday);
 
   const snapPoints = useMemo(() => ["35%", "75%"], []);
 
@@ -34,13 +40,19 @@ export default function AddDueDateBottomSheet({ todoId, bottomSheetModalRef }: P
     backToDetail();
   };
 
+  useEffect(() => {
+    return () => {
+      reset();
+    };
+  }, [reset]);
+
   return (
     <BottomSheetModal ref={bottomSheetModalRef} index={0} snapPoints={snapPoints} backdropComponent={renderBackdrop}>
-      <View className={"flex-1 w-screen p-4 gap-4"}>
+      <View className={"flex-1 w-screen p-4  gap-4"}>
         {/* title */}
         <View className={"flex-row justify-center items-center"}>
           <Text className={"text-xl font-semibold text-center"}>{t("todo.addDueDate.bottomSheet.title")}</Text>
-          <View className={"absolute right-0"}>
+          <View className={"absolute -right-1"}>
             <Button onPress={backToDetail} title={"Done"} />
           </View>
         </View>
@@ -49,12 +61,14 @@ export default function AddDueDateBottomSheet({ todoId, bottomSheetModalRef }: P
         <Pressable className={"flex-row gap-4"} onPress={handlePressToday}>
           <Icon name={"calendarMinus"} size={26} strokeWidth={1.6} />
           <Text className={"text-xl font-normal"}>{t("todo.addDueDate.today")}</Text>
+          <Text className={"text-xl font-normal text-gray-400 ml-auto"}>{days[chosenLanguage][todayDay]}</Text>
         </Pressable>
 
         {/* tomorrow */}
         <View className={"flex-row gap-4"}>
           <Icon name={"calendarCheckOut"} size={26} strokeWidth={1.6} />
           <Text className={"text-xl font-normal"}>{t("todo.addDueDate.tomorrow")}</Text>
+          <Text className={"text-xl font-normal text-gray-400 ml-auto"}>{days[chosenLanguage][tomorrowDay]}</Text>
         </View>
 
         {/* next week */}
---
+++ packages/models/src/date.ts
@@ -0,0 +1,12 @@
+export const days = {
+  en: [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ],
+  ko: ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"],
+}
