32a57197ddcb219c71a44644929e79897a13b214
tkhwang <tkhwang@gmail.com>
2024-08-27 22:34:54 +0900

    🦄 refactor(mobile/hook): refactor to use useFirestore()

--- apps/mobile-app/hooks/queries/useTask.ts
+++ apps/mobile-app/hooks/queries/useTask.ts
@@ -1,29 +1,44 @@
 import { useEffect, useMemo } from "react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { COLLECTIONS, ITask } from "@whatTodo/models";
+import firestore from "@react-native-firebase/firestore";
 
-import { updateTaskCache } from "@/services/Task/updateTaskCache";
+import { IListFS } from "@/types";
+
+import { useFirestore } from "../useFirestore";
 
 export function useTask(taskId: string) {
   const queryClient = useQueryClient();
 
-  const queryKey = useMemo(() => {
+  const key = useMemo(() => {
     return [COLLECTIONS.TASKS, taskId];
   }, [taskId]);
 
+  const { convert, setDoc } = useFirestore();
+
   useEffect(
     function setupTodosEffect() {
-      const unsubscribe = updateTaskCache(taskId, queryClient);
+      const unsubscribe = firestore()
+        .collection(COLLECTIONS.TASKS)
+        .doc(taskId)
+        .onSnapshot((doc) => {
+          const taskDoc = {
+            id: doc.id,
+            ...doc.data(),
+          } as IListFS;
+          const list = convert(taskDoc, doc.id);
+          setDoc(key, list);
+        });
 
       return () => {
         unsubscribe();
       };
     },
-    [queryClient, queryKey, taskId],
+    [queryClient, key, taskId, convert, setDoc],
   );
 
   return useQuery<ITask>({
-    queryKey,
+    queryKey: key,
     queryFn: () => new Promise((): void => {}),
     enabled: !!taskId,
     staleTime: Infinity,
