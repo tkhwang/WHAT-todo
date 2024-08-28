ac20fec3bb5a1457a4c3a05ec951a7c434762d52
tkhwang <tkhwang@gmail.com>
2024-08-28 21:13:33 +0900

    ✨ feat(mobile/hook): implement <TodoList />

--- apps/mobile-app/components/List/TodoList.tsx
+++ apps/mobile-app/components/List/TodoList.tsx
@@ -1,16 +1,27 @@
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 import { useSetAtom } from "jotai";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
+import { ITask } from "@whatTodo/models";
 
 import { Text } from "@/components/ui/text";
 import { currentListIdAtom } from "@/states/list";
+import { useList } from "@/hooks/queries/useList";
+import { useTasks } from "@/hooks/queries/useTasks";
+import Icon from "@/assets/icons";
+
+import TaskListItem from "../Task/TaskListItem";
 
 interface Props {
   listId: string;
 }
 
+const ItemSeparator = () => <View style={{ height: 4 }} />;
+
 export function TodoList({ listId }: Props) {
   const setCurrentListId = useSetAtom(currentListIdAtom);
+
+  const { data: list, isLoading } = useList(listId);
+  const { data: tasks } = useTasks(listId);
 
   useEffect(() => {
     setCurrentListId(listId);
@@ -20,9 +31,26 @@ export function TodoList({ listId }: Props) {
     };
   }, [listId, setCurrentListId]);
 
+  const renderItem = useCallback(({ item, index }: { item: ITask; index: number }) => {
+    return <TaskListItem todo={item} />;
+  }, []);
+
+  if (isLoading) return null;
+
   return (
     <View className={"flex-1"}>
-      <Text className={"text-xl font-normal"}>{listId}</Text>
+      <View className={"flex-row gap-4 items-center py-4"}>
+        <Icon name={"leftToRightListBullet"} size={26} strokeWidth={2} />
+        <Text className={"text-2xl font-semibold"}>{list?.title}</Text>
+      </View>
+      <View style={{ flexShrink: 1 }}>
+        <FlatList
+          data={tasks}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          ItemSeparatorComponent={ItemSeparator}
+        />
+      </View>
     </View>
   );
 }
