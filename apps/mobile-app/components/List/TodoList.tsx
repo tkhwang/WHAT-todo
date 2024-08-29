60af9af083a987b4d8330702acc2e2bd752780d6
tkhwang <tkhwang@gmail.com>
2024-08-29 19:21:28 +0900

    🌈 style(mobile/home): update screen UI

--- apps/mobile-app/components/List/TodoList.tsx
+++ apps/mobile-app/components/List/TodoList.tsx
@@ -1,15 +1,17 @@
 import { FlatList, View } from "react-native";
 import { useSetAtom } from "jotai";
-import { useCallback, useEffect } from "react";
-import { ITask } from "@whatTodo/models";
+import { useCallback, useEffect, useMemo } from "react";
+import { COLLECTIONS, ITask } from "@whatTodo/models";
+import { useCollection } from "react-query-firestore";
+import { useTranslation } from "react-i18next";
 
 import { Text } from "@/components/ui/text";
 import { currentListIdAtom } from "@/states/list";
 import { useList } from "@/hooks/queries/useList";
-import { useTasks } from "@/hooks/queries/useTasks";
 import Icon from "@/assets/icons";
 
 import TaskListItem from "../Task/TaskListItem";
+import { TaskListDoneItem } from "../Task/TaskListDoneItem";
 
 interface Props {
   listId: string;
@@ -18,12 +20,44 @@ interface Props {
 const ItemSeparator = () => <View style={{ height: 4 }} />;
 
 export function TodoList({ listId }: Props) {
+  const { t } = useTranslation();
   const setCurrentListId = useSetAtom(currentListIdAtom);
 
   const { data: list, isLoading } = useList(listId);
 
-  const { data: tasks } = useTasks(listId, false);
-  const { data: doneTasks } = useTasks(listId, true);
+  const { data: tasks } = useCollection(
+    COLLECTIONS.TASKS,
+    {
+      onSuccess: console.log,
+    },
+    {
+      where: [
+        ["listId", "==", listId],
+        ["isDone", "==", false],
+      ],
+    },
+  );
+
+  const sortedTasks = useMemo(() => {
+    return tasks?.sort((a, b) => b.createdAt - a.createdAt);
+  }, [tasks]);
+
+  const { data: doneTasks } = useCollection(
+    COLLECTIONS.TASKS,
+    {
+      onSuccess: console.log,
+    },
+    {
+      where: [
+        ["listId", "==", listId],
+        ["isDone", "==", true],
+      ],
+    },
+  );
+
+  const sortedDoneTasks = useMemo(() => {
+    return doneTasks?.sort((a, b) => b.createdAt - a.createdAt);
+  }, [doneTasks]);
 
   useEffect(() => {
     setCurrentListId(listId);
@@ -35,6 +69,10 @@ export function TodoList({ listId }: Props) {
 
   const renderItem = useCallback(({ item, index }: { item: ITask; index: number }) => {
     return <TaskListItem todo={item} />;
+  }, []);
+
+  const renderDoneItem = useCallback(({ item, index }: { item: ITask; index: number }) => {
+    return <TaskListDoneItem todo={item} />;
   }, []);
 
   if (isLoading) return null;
@@ -50,7 +88,7 @@ export function TodoList({ listId }: Props) {
       {/* tasks list */}
       <View style={{ flexShrink: 1 }}>
         <FlatList
-          data={tasks}
+          data={sortedTasks}
           renderItem={renderItem}
           keyExtractor={(item) => `tasks-list-${item.id}`}
           ItemSeparatorComponent={ItemSeparator}
@@ -60,15 +98,15 @@ export function TodoList({ listId }: Props) {
       {/* List Title */}
       <View className={"flex-row gap-4 items-center py-4"}>
         <Icon name={"leftToRightListBullet"} size={26} strokeWidth={2} />
-        <Text className={"text-2xl font-semibold"}>{list?.title}</Text>
+        <Text className={"text-2xl font-semibold"}>{t("todo.list.isDone")}</Text>
       </View>
 
       {/* tasks list */}
       <View style={{ flexShrink: 1 }}>
         <FlatList
-          data={doneTasks}
-          renderItem={renderItem}
-          keyExtractor={(item) => `tasks-done-list-${item.id}`}
+          data={sortedDoneTasks}
+          renderItem={renderDoneItem}
+          keyExtractor={(item) => `tasks-list-${item.id}`}
           ItemSeparatorComponent={ItemSeparator}
         />
       </View>
