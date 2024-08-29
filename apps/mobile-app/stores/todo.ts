813f7f6828686db06411923d0f8187bb6b7633a2
tkhwang <tkhwang@gmail.com>
2024-08-30 04:32:45 +0900

    ✨ feat(mobile/store): update store

--- apps/mobile-app/stores/todo.ts
+++ apps/mobile-app/stores/todo.ts
@@ -1,11 +1,11 @@
-import { INIT_TASKSTORE_STATE, TaskStoreActions, TaskStoreState } from "@whatTodo/models";
+import { INIT_TASKSTORE_STATE, ITask, TaskStoreActions, TaskStoreState } from "@whatTodo/models";
 import { create } from "zustand";
 
 export const useTaskStore = create<TaskStoreState & TaskStoreActions>((set, get) => ({
   ...INIT_TASKSTORE_STATE,
+  setDueDate: (dueDate: Date) => set({ dueDate }),
+  updateTask: (task: string) => set({ task }),
   setIsLoading: (isLoading: boolean) => set({ isLoading }),
   toggleIsDone: () => set({ isDone: !get().isDone }),
-  updateTask: (task: string) => set({ task }),
-  setDueDate: (dueDate: Date) => set({ dueDate }),
   reset: () => set(INIT_TASKSTORE_STATE),
 }));
--- packages/models/src/store/todo-task.ts
+++ packages/models/src/store/todo-task.ts
@@ -2,21 +2,23 @@ import { ITask } from "../task"
 
 export type TaskStoreState = {
   task: string
+  today: Date
+  dueDate?: Date
   isLoading: boolean
   isDone: boolean
-  dueDate?: Date
 }
 
 export const INIT_TASKSTORE_STATE: TaskStoreState = {
   task: "",
+  today: new Date(),
   isLoading: false,
   isDone: false,
 }
 
 export type TaskStoreActions = {
-  setIsLoading: (isLoading: boolean) => void
-  toggleIsDone: () => void
   updateTask: (task: string) => void
   setDueDate: (dueDate: Date) => void
+  setIsLoading: (isLoading: boolean) => void
+  toggleIsDone: () => void
   reset: () => void
 }
