8c75cb60417a490e42b7b753590855a69467c095
tkhwang <tkhwang@gmail.com>
2024-08-30 20:45:39 +0900

    🦄 refactor(mobile/detail): update detail screen

--- apps/mobile-app/components/Task/TaskDetail.tsx
+++ apps/mobile-app/components/Task/TaskDetail.tsx
@@ -1,4 +1,4 @@
-import { Pressable, TextInput, View } from "react-native";
+import { Keyboard, KeyboardAvoidingView, Platform, Pressable, TextInput, View } from "react-native";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
@@ -45,47 +45,62 @@ export default function TaskDetail({ taskId }: Props) {
   }, [taskId, toggleTaskIsDoneMutation]);
 
   const handleDueDatePress = () => {
+    Keyboard.dismiss();
     init();
     bottomSheetModalRef.current?.present();
+  };
+
+  const handleBlur = () => {
+    Keyboard.dismiss();
   };
 
   if (!task) return null;
 
   return (
-    <View className={"flex-1 flex-col gap-8"}>
-      {/* title */}
-      <View className={"flex-row pt-4 pl-1 gap-4 items-center"}>
-        <Checkbox checked={checked} onCheckedChange={handlePress} />
-        <Text className={cn("text-3xl font-bold", checked ? "line-through" : "")}>{task.task}</Text>
+    <KeyboardAvoidingView
+      className={"flex-1 w-screen"}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View className={"flex-1 flex-col gap-8 justify-end"}>
+        {/* title */}
+        <View className={"flex-row pt-4 pl-1 gap-4 items-center"}>
+          <Checkbox checked={checked} onCheckedChange={handlePress} />
+          <Text className={cn("text-3xl font-bold", checked ? "line-through" : "")}>
+            {task.task}
+          </Text>
+        </View>
+
+        {/* list */}
+        <View className={"flex-row items-center gap-4"}>
+          <Icon name={"leftToRightListBullet"} size={26} strokeWidth={1.6} />
+          <Text className={"text-xl font-normal text-gray-500"}>{t("task.list.title")}</Text>
+          <Text className={"text-xl font-normal text-gray-500"}>{list?.title}</Text>
+        </View>
+
+        {/* due date */}
+        <Pressable className={"flex-row items-center gap-4"} onPress={handleDueDatePress}>
+          <Icon name={"calendar"} size={26} strokeWidth={1.6} />
+          <Text className={"text-xl font-normal text-gray-500"}>{t("todo.addDueDate.title")}</Text>
+        </Pressable>
+
+        {/* note */}
+        <View className={"flex-row items-center gap-4"}>
+          <Icon name={"noteEdit"} size={26} strokeWidth={1.6} />
+          <Textarea
+            ref={inputRef}
+            className={"w-full"}
+            placeholder={t("task.note.addNote.placehold")}
+            value={note}
+            onBlur={handleBlur}
+            onChangeText={setNote}
+            autoFocus={false}
+            aria-labelledby={"textareaLabel"}
+          />
+        </View>
+        <View className={"flex-1"} />
+
+        <AddDueDateBottomSheet todoId={taskId} bottomSheetModalRef={bottomSheetModalRef} />
       </View>
-
-      {/* list */}
-      <View className={"flex-row items-center gap-4"}>
-        <Icon name={"leftToRightListBullet"} size={26} strokeWidth={1.6} />
-        <Text className={"text-xl font-normal text-gray-500"}>{t("task.list.title")}</Text>
-        <Text className={"text-xl font-normal text-gray-500"}>{list?.title}</Text>
-      </View>
-
-      {/* due date */}
-      <Pressable className={"flex-row items-center gap-4"} onPress={handleDueDatePress}>
-        <Icon name={"calendar"} size={26} strokeWidth={1.6} />
-        <Text className={"text-xl font-normal text-gray-500"}>{t("todo.addDueDate.title")}</Text>
-      </Pressable>
-
-      {/* note */}
-      <Pressable className={"flex-row items-center gap-4"} onPress={handleDueDatePress}>
-        <Icon name={"noteEdit"} size={26} strokeWidth={1.6} />
-        <Textarea
-          ref={inputRef}
-          className={"flex-1"}
-          placeholder={t("task.note.addNote.placehold")}
-          value={note}
-          onChangeText={setNote}
-          aria-labelledby={"textareaLabel"}
-        />
-      </Pressable>
-
-      <AddDueDateBottomSheet todoId={taskId} bottomSheetModalRef={bottomSheetModalRef} />
-    </View>
+    </KeyboardAvoidingView>
   );
 }
