230f85e89c2ad336dd618c88468595538fefae9a
tkhwang <tkhwang@gmail.com>
2024-08-30 18:16:05 +0900

    🦄 refactor(mobile/hook): rename to useActiveTasks()

--- apps/mobile-app/hooks/queries/useActiveTasks.ts
+++ apps/mobile-app/hooks/queries/useActiveTasks.ts
@@ -3,16 +3,17 @@ import { useEffect, useMemo } from "react";
 import { COLLECTIONS, ITask } from "@whatTodo/models";
 import firestore from "@react-native-firebase/firestore";
 
-import { useUserTasks } from "./useUserTasks";
-import { useFirestore } from "../useFirestore";
 import { ITaskFS } from "@/types";
 
-export function useTasks(listId: string, isDone = false) {
+import { useUserTasks } from "./useUserTasks";
+import { useFirestore } from "../useFirestore";
+
+export function useActiveTasks(listId: string) {
   const queryClient = useQueryClient();
 
   const { data: userTasks } = useUserTasks(listId);
 
-  const { convert, getDoc, setDoc } = useFirestore<ITaskFS, ITask>();
+  const { convert, setDoc } = useFirestore<ITaskFS, ITask>();
 
   const taskIds = useMemo(() => {
     return (userTasks ?? []).map((userTodo) => userTodo.id);
@@ -23,7 +24,7 @@ export function useTasks(listId: string, isDone = false) {
       const unsubscribes: (() => void)[] = [];
       // eslint-disable-next-line no-restricted-syntax
       for (const taskId of taskIds ?? []) {
-        const key = [COLLECTIONS.TASKS, taskId];
+        const key = [COLLECTIONS.TASKS, taskId, COLLECTIONS.IS_DONE, false];
         const unsubscribe = firestore()
           .collection(COLLECTIONS.TASKS)
           .doc(taskId)
@@ -33,7 +34,9 @@ export function useTasks(listId: string, isDone = false) {
               ...doc.data(),
             } as ITaskFS;
             const task = convert(taskDoc, doc.id);
-            setDoc(key, task);
+            if (!task.isDone) {
+              setDoc(key, task);
+            }
           });
 
         unsubscribes.push(unsubscribe);
@@ -43,13 +46,13 @@ export function useTasks(listId: string, isDone = false) {
         unsubscribes.forEach((unsubscribe) => unsubscribe());
       };
     },
-    [queryClient, taskIds],
+    [convert, queryClient, setDoc, taskIds],
   );
 
   return useQueries({
     queries:
       taskIds?.map((taskId) => ({
-        queryKey: [COLLECTIONS.TASKS, taskId],
+        queryKey: [COLLECTIONS.TASKS, taskId, COLLECTIONS.IS_DONE, false],
         queryFn: () => new Promise<ITask>((): void => {}),
         enabled: !!taskId,
         staleTime: Infinity,
