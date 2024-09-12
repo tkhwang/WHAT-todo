edda243b9562641d2cb942ff1dbc3b96caec3753
tkhwang <tkhwang@gmail.com>
2024-09-12 21:48:43 +0900

    ✨ feat(mobile/debug): config react query devTools

--- apps/mobile-app/config/ReactQueryClientProvider.tsx
+++ apps/mobile-app/config/ReactQueryClientProvider.tsx
@@ -1,10 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useReactQueryDevTools } from "@dev-plugins/react-query";
+import React from "react";
 
 interface Props {
   children: React.ReactNode;
 }
 export function ReactQueryClientProvider({ children }: Props) {
   const queryClient = new QueryClient();
+  useReactQueryDevTools(queryClient);
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
