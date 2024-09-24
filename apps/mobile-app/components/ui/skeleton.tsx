ce8b87f49b0c680590b99b9111342373d3a9c47e
tkhwang <tkhwang@gmail.com>
2024-09-24 23:17:42 +0900

    ✨ feat(mobile/ui): add rmr ui

--- apps/mobile-app/components/ui/skeleton.tsx
+++ apps/mobile-app/components/ui/skeleton.tsx
@@ -11,18 +11,30 @@ import { cn } from "@/lib/utils";
 
 const duration = 1000;
 
-function Skeleton({ className, ...props }: Omit<React.ComponentPropsWithoutRef<typeof Animated.View>, "style">) {
+function Skeleton({
+  className,
+  ...props
+}: Omit<React.ComponentPropsWithoutRef<typeof Animated.View>, "style">) {
   const sv = useSharedValue(1);
 
   React.useEffect(() => {
-    sv.value = withRepeat(withSequence(withTiming(0.5, { duration }), withTiming(1, { duration })), -1);
+    sv.value = withRepeat(
+      withSequence(withTiming(0.5, { duration }), withTiming(1, { duration })),
+      -1,
+    );
   }, []);
 
   const style = useAnimatedStyle(() => ({
     opacity: sv.value,
   }));
 
-  return <Animated.View style={style} className={cn("rounded-md bg-secondary dark:bg-muted", className)} {...props} />;
+  return (
+    <Animated.View
+      style={style}
+      className={cn("rounded-md bg-secondary dark:bg-muted", className)}
+      {...props}
+    />
+  );
 }
 
 export { Skeleton };
