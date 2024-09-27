5e2343f354db777933c8d6096b5f2159d87ce323
tkhwang <tkhwang@gmail.com>
2024-09-27 23:11:12 +0900

    ✨ feat(mobile/task): apply state lift up in <TaskTypeSelect />

--- apps/mobile-app/components/Task/TaskTypeSelect.tsx
+++ apps/mobile-app/components/Task/TaskTypeSelect.tsx
@@ -1,22 +1,20 @@
 import { View } from "react-native";
 import { useTranslation } from "react-i18next";
+import { TaskType } from "@whatTodo/models";
 
 import { Text } from "@/components/ui/text";
 import Icon from "@/assets/icons";
 import { appTheme } from "@/constants/uiConsts";
-import { cn } from "@/lib/utils";
-import { useTaskStore } from "@/stores/todo";
 
 import { Switch } from "../ui/switch";
 
-export default function TaskTypeSelect() {
+interface Props {
+  taskType: TaskType;
+  toggleTaskType: () => void;
+}
+
+export default function TaskTypeSelect({ taskType, toggleTaskType }: Props) {
   const { t } = useTranslation();
-
-  const { taskType, setTaskType } = useTaskStore();
-
-  const handleToggleTaskType = () => {
-    setTaskType(taskType === "todo" ? "not-todo" : "todo");
-  };
 
   return (
     <View className={"flex flex-row items-center justify-center gap-1"}>
@@ -30,12 +28,10 @@ export default function TaskTypeSelect() {
           color={taskType === "todo" ? appTheme.colors.textLight : appTheme.colors.primary}
         />
       </View>
+
       {/* Center: SWITCH */}
-      <Switch
-        checked={taskType === "todo"}
-        onCheckedChange={handleToggleTaskType}
-        nativeID={"todo"}
-      />
+      <Switch checked={taskType === "todo"} onCheckedChange={toggleTaskType} nativeID={"todo"} />
+
       {/* RIGHT: TODO */}
       <View className={"flex flex-row items-center gap-1 bg-gray-200 px-3 py-2 rounded-full"}>
         <Icon
