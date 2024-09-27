f551d273f3ed203486bde1359d95e859438d4080
tkhwang <tkhwang@gmail.com>
2024-09-28 08:29:15 +0900

    🦄 refactor(mobile/hook): refactor to use useListsQueryOptions()

--- apps/mobile-app/hooks/queries/useLists.ts
+++ apps/mobile-app/hooks/queries/useLists.ts
@@ -1,50 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import { useEffect, useMemo } from "react";
-import firestore from "@react-native-firebase/firestore";
-import { COLLECTIONS, IList } from "@whatTodo/models";
-
-import { IListFS } from "@/types";
+import { IList } from "@whatTodo/models";
 
 import { useUserLists } from "./useUserLists";
-import { useFirestore } from "../useFirestore";
+import { useListsQueryOptions } from "./queryOptions/useListsQueryOptions";
 
 export function useLists<TSelected = IList[]>(select?: (lists: IList[]) => TSelected) {
   const { data: userLists } = useUserLists();
 
-  const { convert, setDoc, setDocs } = useFirestore();
-
-  const listIds = useMemo(() => {
-    return (userLists ?? []).map((userList) => userList.id);
-  }, [userLists]);
-
-  useEffect(
-    function useListsEffect() {
-      if (!listIds.length) return undefined;
-
-      const key = [COLLECTIONS.LISTS];
-      const unusbscribe = firestore()
-        .collection(COLLECTIONS.LISTS)
-        .where(firestore.FieldPath.documentId(), "in", listIds)
-        .onSnapshot((snapshot) => {
-          if (!snapshot) return;
-
-          const lists = snapshot.docs.map((doc) => {
-            const listDoc = doc.data() as IListFS;
-            return convert(listDoc, doc.id);
-          });
-          setDocs(key, lists);
-        });
-
-      return () => {
-        unusbscribe();
-      };
-    },
-    [convert, listIds, setDoc, setDocs],
-  );
-
   return useQuery<IList[], Error, TSelected>({
-    queryKey: [COLLECTIONS.LISTS],
-    queryFn: () => new Promise((): void => {}),
+    ...useListsQueryOptions(),
     select,
     enabled: !!userLists && userLists.length > 0,
     staleTime: Infinity,
