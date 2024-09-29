c54fb35ee624badabde71f34aee13e55768b1e33
tkhwang <tkhwang@gmail.com>
2024-09-29 15:57:06 +0900

    ✨ feat(mobile/hook): add getUsersBySearchText() query hook

---
+++ apps/mobile-app/hooks/queries/useSearchUsers.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { APIS } from "@whatTodo/models";
+
+import { httpClient } from "@/utils";
+
+const getUsersBySearchText = async (searchText?: string) => {
+  if (!searchText) return [];
+
+  const { data } = await httpClient.get(`/users/search?searchText=${searchText}`);
+  return data;
+};
+
+export function useSearchUsers(searchText?: string) {
+  return useQuery({
+    queryKey: [APIS.users.searchUsers],
+    queryFn: () => getUsersBySearchText(searchText),
+    enabled: !!searchText,
+    select: (data) => data.slice(0, 10),
+  });
+}
