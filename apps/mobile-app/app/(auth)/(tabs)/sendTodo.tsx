190d6ccfee24df12842fa17a4f590fc9b98014ea
tkhwang <tkhwang@gmail.com>
2024-09-29 16:09:43 +0900

    ✨ feat(mobile/sendTodo): add search user UI

--- apps/mobile-app/app/(auth)/(tabs)/sendTodo.tsx
+++ apps/mobile-app/app/(auth)/(tabs)/sendTodo.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key */
 import { View } from "react-native";
 import { useTranslation } from "react-i18next";
 import { useRef, useState } from "react";
@@ -7,17 +8,23 @@ import ScreenWrapper from "@/components/MainLayout/ScreenWrapper";
 import Header from "@/components/MainLayout/Header";
 import Input from "@/components/Input";
 import Icon from "@/assets/icons";
+import { useSearchUsers } from "@/hooks/queries/useSearchUsers";
+import { IUserFS } from "@/types";
 
 export default function SendTodo() {
   const { t } = useTranslation();
 
-  const [searchText, setSearchText] = useState("");
   const inputRef = useRef(null);
+  const [searchText, setSearchText] = useState("");
+
+  const { data: searchedUsers } = useSearchUsers(searchText);
+
+  const onBackPress = () => setSearchText("");
 
   return (
     <ScreenWrapper>
       <View className={"flex flex-1 flex-col p-4 gap-4"}>
-        <Header title={t("title.expert.sendTodo")} showBackButton />
+        <Header title={t("title.expert.sendTodo")} showBackButton onBackPress={onBackPress} />
         {/* Search User */}
         <Text className={"text-xl font-semibold"}>
           {`${t("title.expert.sendTodo.select.user.text")}:`}
@@ -33,6 +40,15 @@ export default function SendTodo() {
           value={searchText}
           fontSize={18}
         />
+        <View className={"flex flex-col"}>
+          {(searchedUsers ?? []).map((user: IUserFS, index: number) => (
+            <View key={index} className={"flex py-2"}>
+              <Text key={`searched-user-name-${index}-${user.id}`}>{user.name}</Text>
+              <Text key={`searched-user-email-${index}-${user.id}`}>{user.email}</Text>
+              <Text key={`searched-user-whatTodoId-${index}-${user.id}`}>{user.whatTodoId}</Text>
+            </View>
+          ))}
+        </View>
       </View>
     </ScreenWrapper>
   );
