7f685b9e7aaa56dc38df7c7119d1b003a62def93
tkhwang <tkhwang@gmail.com>
2024-10-01 13:54:49 +0900

    🦄 refactor(mobile/component): refactor to use <RadioSwitch />

--- apps/mobile-app/components/Task/TaskTypeSelect.tsx
+++ apps/mobile-app/components/Task/TaskTypeSelect.tsx
@@ -1,14 +1,10 @@
-import { View } from "react-native";
 import { useTranslation } from "react-i18next";
 import { TaskType } from "@whatTodo/models";
 
-import { Text } from "@/components/ui/text";
 import Icon from "@/assets/icons";
 import { appTheme } from "@/constants/uiConsts";
-import { useColorScheme } from "@/lib/useColorScheme";
-import { cn } from "@/lib/utils";
 
-import { Switch } from "../ui/switch";
+import RadioSwitch from "../Switch/RadioSwitch";
 
 interface Props {
   taskType: TaskType;
@@ -18,44 +14,31 @@ interface Props {
 export default function TaskTypeSelect({ taskType, toggleTaskType }: Props) {
   const { t } = useTranslation();
 
-  const { isDarkColorScheme } = useColorScheme();
-
   return (
-    <View className={"flex flex-row items-center justify-center gap-1"}>
-      {/* Left: DON'T DO */}
-      <View
-        className={cn(
-          "flex flex-row items-center gap-1 px-3 py-2 rounded-full",
-          isDarkColorScheme ? "bg-gray-800" : "bg-gray-200",
-        )}
-      >
-        <Text className={"text-sm font-normal"}>{t("task.list.type.notTodo")}</Text>
-        <Icon
-          name={"noteRemove"}
-          size={26}
-          strokeWidth={1.6}
-          color={taskType === "todo" ? appTheme.colors.textLight : appTheme.colors.primary}
-        />
-      </View>
-
-      {/* Center: SWITCH */}
-      <Switch checked={taskType === "todo"} onCheckedChange={toggleTaskType} nativeID={"todo"} />
-
-      {/* RIGHT: TODO */}
-      <View
-        className={cn(
-          "flex flex-row items-center gap-1 px-3 py-2 rounded-full",
-          isDarkColorScheme ? "bg-gray-800" : "bg-gray-200",
-        )}
-      >
+    <RadioSwitch
+      switchState={taskType === "todo"}
+      toggleSwitchState={toggleTaskType}
+      switchStateNativeId={"todo"}
+      // truthy
+      truthyText={t("task.list.type.todo")}
+      TruthyIcon={
         <Icon
           name={"checkmarkSquare"}
           size={26}
           strokeWidth={1.6}
           color={taskType === "todo" ? appTheme.colors.secondary : appTheme.colors.textLight}
         />
-        <Text className={"text-sm font-normal"}>{t("task.list.type.todo")}</Text>
-      </View>
-    </View>
+      }
+      // falsy
+      falseText={t("task.list.type.notTodo")}
+      FalsyIcon={
+        <Icon
+          name={"noteRemove"}
+          size={26}
+          strokeWidth={1.6}
+          color={taskType === "todo" ? appTheme.colors.textLight : appTheme.colors.primary}
+        />
+      }
+    />
   );
 }
