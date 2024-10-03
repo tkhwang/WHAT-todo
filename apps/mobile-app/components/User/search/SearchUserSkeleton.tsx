28bb889382ced319a4dbec2c4408757eed769a6d
tkhwang <tkhwang@gmail.com>
2024-10-03 16:19:14 +0900

    ✨ feat(mobile/sendTodo): change icon using skeleton image

--- apps/mobile-app/components/User/search/SearchUserSkeleton.tsx
+++ apps/mobile-app/components/User/search/SearchUserSkeleton.tsx
@@ -11,15 +11,15 @@ export default function SearchUserSkeleton({ index }: Props) {
   return (
     <View key={index} className={"flex py-2"}>
       <View className={"flex flex-row gap-2"}>
-        <Icon name={"user"} size={20} strokeWidth={1.6} />
+        <Skeleton className={"h-5 w-5 rounded-full"} />
         <Skeleton className={"h-4 w-[100px]"} />
       </View>
       <View className={"flex flex-row gap-2"}>
-        <Icon name={"passport"} size={20} strokeWidth={1.6} />
+        <Skeleton className={"h-5 w-5 rounded-full"} />
         <Skeleton className={"h-4 w-[180px]"} />
       </View>
       <View className={"flex flex-row gap-2"}>
-        <Icon name={"mailAtSign"} size={20} strokeWidth={1.6} />
+        <Skeleton className={"h-5 w-5 rounded-full"} />
         <Skeleton className={"h-4 w-[290px]"} />
       </View>
     </View>
