0b52767956b6ed18dbbe214ded9dab8828b6028b
tkhwang <tkhwang@gmail.com>
2024-10-03 17:32:28 +0900

    ✨ feat(backend/todo): setup todo module

--- apps/backend/src/todos/todos.module.ts
+++ apps/backend/src/todos/todos.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
+import { FirebaseModule } from 'src/firebase/firebase.module';
 
 @Module({
+  imports: [FirebaseModule],
   controllers: [TodosController],
-  providers: [TodosService]
+  providers: [TodosService],
 })
 export class TodosModule {}
