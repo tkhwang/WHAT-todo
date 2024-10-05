962c40a8a1f8251c43db5a23f10731dd84825400
tkhwang <tkhwang@gmail.com>
2024-10-05 22:13:51 +0900

    ✨ feat(hook): add select in useUserTasks

--- apps/mobile-app/hooks/queries/useUserTasks.ts
+++ apps/mobile-app/hooks/queries/useUserTasks.ts
@@ -7,14 +7,18 @@ import { myUserIdAtom } from "@/states/me";
 import { getUserTasksQueryOptions } from "./queryOptions/getUserTasksQueryOptions";
 import { useUserTasksSideEffect } from "./sideEffect/useUserTasksSideEffect";
 
-export function useUserTasks(listId: string) {
+export function useUserTasks<TSelected = IUserTask[]>(
+  listId: string,
+  select?: (data: IUserTask[]) => TSelected,
+) {
   const myUserId = useAtomValue(myUserIdAtom);
 
   useUserTasksSideEffect(listId);
 
-  return useQuery<IUserTask[]>({
+  return useQuery<IUserTask[], Error, TSelected>({
     ...getUserTasksQueryOptions(myUserId, listId),
     enabled: !!myUserId,
     staleTime: Infinity,
+    select,
   });
 }
