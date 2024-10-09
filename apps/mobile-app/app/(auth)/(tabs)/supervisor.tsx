d6167c52c0da820679a9c27bb3919d615d3a282a
tkhwang <tkhwang@gmail.com>
2024-10-09 21:04:06 +0900

    ✨ feat(mobile/supervisor): add <ListContainer /> for supervisor

--- apps/mobile-app/app/(auth)/(tabs)/supervisor.tsx
+++ apps/mobile-app/app/(auth)/(tabs)/supervisor.tsx
@@ -1,20 +1,19 @@
 import { View } from "react-native";
 import { useTranslation } from "react-i18next";
 
-import { Text } from "@/components/ui/text";
 import ScreenWrapper from "@/components/MainLayout/ScreenWrapper";
 import MainHeader from "@/components/MainLayout/MainHeader";
-import FloatingActionButton from "@/components/Button/FloatingActionButton";
+import ListContainer from "@/components/List/ListContainer";
 
 export default function TabThreeScreen() {
   const { t } = useTranslation();
   return (
     <ScreenWrapper>
       <MainHeader />
-      <View className={"flex-1 justify-center items-center"}>
-        <Text className={"text-xl font-semibold"}>{t("app.screen.supervisor")}</Text>
+
+      <View className={"flex-1 w-screen justify-end"}>
+        <ListContainer userType={"supervisor"} />
       </View>
-      {/* <FloatingActionButton /> */}
     </ScreenWrapper>
   );
 }
