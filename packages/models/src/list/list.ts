6d8869a811d60bd14f28683ea865cbfeb81dbea0
tkhwang <tkhwang@gmail.com>
2024-10-10 21:33:59 +0900

    ✨ feat(models/type): add listType in IList

--- packages/models/src/list/list.ts
+++ packages/models/src/list/list.ts
@@ -1,8 +1,15 @@
+import { UserType } from "../user"
+
 export const DEFAULT_LIST_TITLE = "My Todo"
+
+export type ListType = "defaultUserList"
 
 export interface IListCommon {
   id: string
   title: string
+  userId: string
+  userType: UserType
+  listType?: ListType
 }
 
 export interface IList extends IListCommon {
