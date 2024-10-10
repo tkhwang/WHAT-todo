c62f7936898d649b03fc2f5ec8d47a8a6964fb02
tkhwang <tkhwang@gmail.com>
2024-10-10 21:49:35 +0900

    ✨ feat(mobile/home): add to set userDefaultListId in <Index />

--- apps/mobile-app/app/(auth)/(tabs)/index.tsx
+++ apps/mobile-app/app/(auth)/(tabs)/index.tsx
@@ -1,17 +1,30 @@
 import { useEffect, useRef } from "react";
 import { KeyboardAvoidingView, Platform, TextInput, View } from "react-native";
+import { useSetAtom } from "jotai";
 
 import ScreenWrapper from "@/components/MainLayout/ScreenWrapper";
 import MainHeader from "@/components/MainLayout/MainHeader";
 import AddTaskInput from "@/components/Task/add/AddTaskInput";
 import ListContainer from "@/components/List/ListContainer";
+import { useSelectUserDefaultList } from "@/hooks/queries/select/useSelectUserDefaultList";
+import { useUserLists } from "@/hooks/queries/useUserLists";
+import { userDefaultListIdAtom } from "@/states/list";
 
 export default function HomeScreen() {
   const inputRef = useRef<TextInput>(null);
 
+  const { selectUserDefaultList } = useSelectUserDefaultList();
+  const { data: userDefaultList } = useUserLists("user", selectUserDefaultList);
+
+  const setUserDefaultListId = useSetAtom(userDefaultListIdAtom);
+
   useEffect(() => {
     inputRef.current?.blur();
   }, []);
+
+  useEffect(() => {
+    if (userDefaultList?.id) setUserDefaultListId(userDefaultList.id);
+  }, [setUserDefaultListId, userDefaultList]);
 
   return (
     <ScreenWrapper>
