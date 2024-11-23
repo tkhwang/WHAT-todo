f8cfb92cbd3c92e2a42d8fa9b5f07b80d02facfe
tkhwang <tkhwang@gmail.com>
2024-11-23 21:57:21 +0900

    ✨ feat(web/component): expand button variants and styles
    - add new button variants: primary, secondary, ghost, muted, and teritary
    - update button styles to use gradient backgrounds and improve visual hierarchy

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,11 +3,14 @@ import Image from "next/image"
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="destructive" size="sm">
-        Destructive
-      </Button>
-      Hellow World
+    <div className="flex gap-4">
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
     </div>
   )
 }
