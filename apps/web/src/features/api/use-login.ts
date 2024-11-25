b7306f86a4811f709708ba23f2c7637b5ec1cbbb
tkhwang <tkhwang@gmail.com>
2024-11-26 08:29:20 +0900

    ♻️ refactor(web/auth): correct mutation function parameter destructuring
    - fix type definition for RequestType
    - improve type inference by updating the destructuring of the function parameter in useMutation

--- apps/web/src/features/api/use-login.ts
+++ apps/web/src/features/api/use-login.ts
@@ -3,14 +3,12 @@ import { InferRequestType, InferResponseType } from "hono"
 
 import { client } from "@/lib/rpc"
 
-type RequestType = InferRequestType<
-  (typeof client.api.auth.login)["$post"]
->["json"]
+type RequestType = InferRequestType<(typeof client.api.auth.login)["$post"]>
 type ResponseType = InferResponseType<(typeof client.api.auth.login)["$post"]>
 
 export const useLogin = () => {
   const mutation = useMutation<ResponseType, Error, RequestType>({
-    mutationFn: async (json) => {
+    mutationFn: async ({ json }) => {
       const response = await client.api.auth.login["$post"]({ json })
       return await response.json()
     },
