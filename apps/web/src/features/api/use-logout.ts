52d9858223a2cebcb67e0db0b91b84a2dadacb40
tkhwang <tkhwang@gmail.com>
2024-11-28 20:58:51 +0900

    ✨ feat(web/auth): refresh page on successful logout using useRouter
    - import useRouter from next/navigation to manipulate routing
    - add router.refresh() to onSuccess callback of useMutation

--- apps/web/src/features/api/use-logout.ts
+++ apps/web/src/features/api/use-logout.ts
@@ -2,10 +2,12 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
 
 type ResponseType = InferResponseType<(typeof client.api.auth.logout)["$post"]>;
 
 export const useLogout = () => {
+  const router = useRouter();
   const queryClient = useQueryClient();
 
   const mutation = useMutation<ResponseType, Error>({
@@ -14,6 +16,7 @@ export const useLogout = () => {
       return await response.json();
     },
     onSuccess: () => {
+      router.refresh();
       queryClient.invalidateQueries({ queryKey: ["current"] });
     },
   });
