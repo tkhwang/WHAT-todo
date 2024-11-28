6455d125710ac834c2822771d2bf9080bc56aade
tkhwang <tkhwang@gmail.com>
2024-11-28 21:56:04 +0900

    ✨ feat(web/layout): add new dashboard layout component
    - restructure dashboard page by moving it to a new directory
    - remove user button from the home page
    - add navbar and sidebar components to the dashboard layout

--- apps/web/src/app/(dashboard)/page.tsx
+++ apps/web/src/app/(dashboard)/page.tsx
@@ -1,5 +1,5 @@
 import { getCurrent } from "@/features/auth/actions";
-import { UserButton } from "@/features/components/user-button";
+
 import { redirect } from "next/navigation";
 
 export default async function Home() {
@@ -7,9 +7,5 @@ export default async function Home() {
 
   if (!user) redirect("/sign-in");
 
-  return (
-    <div>
-      <UserButton />
-    </div>
-  );
+  return <div>This is a home page</div>;
 }
