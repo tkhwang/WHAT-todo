9f929b0c36a0e1344149a6536c9ff7514febf073
tkhwang <tkhwang@gmail.com>
2024-11-28 20:58:19 +0900

    ✨ feat(web/auth): add user profile and logout functionality

---
+++ apps/web/src/features/components/user-button.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Loader, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DottedSeparator } from "@/components/dotted-separator";
+
+import { useLogout } from "../api/use-logout";
+import { useCurrent } from "../api/use-current";
+
+export const UserButton = () => {
+  const { mutate: logout } = useLogout();
+  const { data: user, isLoading } = useCurrent();
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center border rounded-full size-10 bg-neutral-200 border-neutral-300">
+        <Loader className="size-4 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+
+  if (!user) {
+    return null;
+  }
+
+  const { name, email } = user;
+
+  const avatarFallback = name ? name.charAt(0).toUpperCase() : email.charAt(0).toUpperCase() ?? "U";
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="relative outline-none">
+        <Avatar className="transition border size-10 hover:opacity-75 border-neutral-300">
+          <AvatarFallback className="flex items-center justify-center font-medium bg-neutral-200 text-neutral-500">
+            {avatarFallback}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" side="bottom" className="w-60" sideOffset={10}>
+        <div className="flex flex-col items-center justify-center gap-2 px-2.5 py-4">
+          <Avatar className="size-[52px] border border-neutral-300">
+            <AvatarFallback className="flex items-center justify-center text-xl font-medium bg-neutral-200 text-neutral-500">
+              {avatarFallback}
+            </AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-sm font-medium text-neutral-900">{name || "User"}</p>
+            <p className="text-xs text-neutral-500">{email}</p>
+          </div>
+        </div>
+        <DottedSeparator className="mb-1" />
+        <DropdownMenuItem
+          onClick={() => logout()}
+          className="flex items-center justify-center h-10 font-medium cursor-pointer text-amber-700"
+        >
+          <LogOut className="mr-2 size-4" />
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
