da1ef6011f4ba547f938158e7d0d9f0eba0bec4a
tkhwang <tkhwang@gmail.com>
2024-11-28 21:56:42 +0900

    ✨ feat(web/layout): add responsive mobile sidebar component
    - introduce `MobileSidebar` utilizing `Sheet` for improved navigation on mobile devices

---
+++ apps/web/src/components/mobile-sidebar.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Sidebar } from "./sidebar";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+
+export function MobileSidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
+  return (
+    <Sheet modal={false} open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger asChild>
+        <Button variant="secondary" className="lg:hidden">
+          <MenuIcon className="size-4 text-neutral-500" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+}
