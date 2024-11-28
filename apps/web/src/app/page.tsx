c20c8c8daaabca4b1f5362fa38d55aacb4c28e99
tkhwang <tkhwang@gmail.com>
2024-11-28 20:59:03 +0900

    ✨ feat(web/page): integrate server-side user authentication
    - replace client-side hooks with server-side `getCurrent` authentication
    - use `redirect` from server-side to navigate unauthenticated users
    - add `UserButton` component for improved user navigation interface

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,39 +1,15 @@
-"use client";
+import { getCurrent } from "@/features/auth/actions";
+import { UserButton } from "@/features/components/user-button";
+import { redirect } from "next/navigation";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+export default async function Home() {
+  const user = await getCurrent();
 
-import { useCurrent } from "@/features/api/use-current";
-import { useLogout } from "@/features/api/use-logout";
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  const router = useRouter();
-
-  const { data, isLoading } = useCurrent();
-
-  const { mutate: logout } = useLogout();
-
-  useEffect(() => {
-    if (!data && !isLoading) {
-      router.push("/sign-in");
-    }
-  }, [data, isLoading, router]);
+  if (!user) redirect("/sign-in");
 
   return (
-    // <main className="flex flex-col items-center justify-center min-h-screen gap-10 p-24">
-    //   <h1 className="text-4xl font-bold">WHAT-todo-dev</h1>
-    //   Todo from experts with supervisors
-    //   <Image
-    //     src="/images/team-checklist-pana.png"
-    //     alt="what-todo banner image"
-    //     width={400}
-    //     height={200}
-    //   />
-    // </main>
     <div>
-      Only visible to logged in users
-      <Button onClick={() => logout()}>Logout</Button>
+      <UserButton />
     </div>
   );
 }
