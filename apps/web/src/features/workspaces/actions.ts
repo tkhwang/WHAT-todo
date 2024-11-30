3ece7b9551af3a7b1059416e2bc4fe39603f3c0a
tkhwang <tkhwang@gmail.com>
2024-11-30 15:41:56 +0900

    ✨ feat(web/workspaces/actions): add function to retrieve user workspaces from database
    - integrate appwrite client for database interaction
    - handle user session and filter workspaces by member association

---
+++ apps/web/src/features/workspaces/actions.ts
@@ -0,0 +1,41 @@
+import { Account, Client, Databases, Query } from "node-appwrite";
+import { cookies } from "next/headers";
+import { AUTH_COOKIE } from "../auth/constants";
+import { MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { DATABASE_ID } from "@/config";
+
+export async function getWorkSpaces() {
+  try {
+    const client = new Client()
+      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+
+    const session = (await cookies()).get(AUTH_COOKIE);
+
+    if (!session) return null;
+
+    client.setSession(session.value);
+    const databases = new Databases(client);
+    const account = new Account(client);
+    const user = await account.get();
+
+    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+      Query.equal("userId", user.$id),
+    ]);
+
+    if (members.total === 0) {
+      return { documents: [], total: 0 };
+    }
+
+    const workspaceIds = members.documents.map((member) => member.workspaceId);
+
+    const workspaces = await databases.listDocuments(DATABASE_ID, WORKSPACES_ID, [
+      Query.orderDesc("$createdAt"),
+      Query.contains("$id", workspaceIds),
+    ]);
+
+    return workspaces;
+  } catch {
+    return { documents: [], total: 0 };
+  }
+}
