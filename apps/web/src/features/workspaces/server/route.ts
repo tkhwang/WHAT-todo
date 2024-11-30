0911ed267cb28c930f10665e50c664306a1e5a03
tkhwang <tkhwang@gmail.com>
2024-11-30 14:12:48 +0900

    ✨ feat(web/workspace): add image upload feature for workspaces
    - integrate file input for image uploads with drag-and-drop support
    - handle image previews and deletions within form
    - update server route to process image uploads and store URLs

--- apps/web/src/features/workspaces/server/route.ts
+++ apps/web/src/features/workspaces/server/route.ts
@@ -1,23 +1,35 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { createWorkspaceSchema } from "../schema";
+import { createWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
 import { ID } from "node-appwrite";
-import { DATABASE_ID, WORKSPACES_ID } from "@/config";
+import { DATABASE_ID, IMAGE_BUCKET_ID, WORKSPACES_ID } from "@/config";
 
 const app = new Hono().post(
   "/",
-  zValidator("json", createWorkspaceSchema),
+  zValidator("form", createWorkspaceSchema),
   sessionMiddleware,
   async (c) => {
     const databases = c.get("databases");
+    const storage = c.get("storage");
     const user = c.get("user");
 
-    const { name } = c.req.valid("json");
+    const { name, image } = c.req.valid("form");
+
+    let uploadImageUrl: string | undefined;
+
+    if (image instanceof File) {
+      const file = await storage.createFile(IMAGE_BUCKET_ID, ID.unique(), image);
+
+      const arrayBuffer = await storage.getFilePreview(IMAGE_BUCKET_ID, file.$id);
+
+      uploadImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
+    }
 
     const workspace = await databases.createDocument(DATABASE_ID, WORKSPACES_ID, ID.unique(), {
       name,
       userId: user.$id,
+      imageUrl: uploadImageUrl,
     });
 
     return c.json({ data: workspace });
