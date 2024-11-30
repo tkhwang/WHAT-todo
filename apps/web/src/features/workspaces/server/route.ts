14b140b57d3d36e5c9d4bcf415be17d30f640238
tkhwang <tkhwang@gmail.com>
2024-11-30 15:14:21 +0900

    ✨ feat(web/workspace): implement member roles and workspace invite codes
    - utilize `Query` to filter workspaces by member association
    - generate invite codes for new workspaces
    - create member document with admin role upon workspace creation

--- apps/web/src/features/workspaces/server/route.ts
+++ apps/web/src/features/workspaces/server/route.ts
@@ -2,14 +2,29 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { ID } from "node-appwrite";
-import { DATABASE_ID, IMAGE_BUCKET_ID, WORKSPACES_ID } from "@/config";
+import { ID, Query } from "node-appwrite";
+import { DATABASE_ID, IMAGE_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { MemberRole } from "@/features/members/types";
+import { generateInviteCode } from "@/lib/utils";
 
 const app = new Hono()
   .get("/", sessionMiddleware, async (c) => {
+    const user = c.get("user");
     const databases = c.get("databases");
+    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+      Query.equal("userId", user.$id),
+    ]);
 
-    const workspaces = await databases.listDocuments(DATABASE_ID, WORKSPACES_ID);
+    if (members.total === 0) {
+      return c.json({ data: { documents: [], total: 0 } });
+    }
+
+    const workspaceIds = members.documents.map((member) => member.workspaceId);
+
+    const workspaces = await databases.listDocuments(DATABASE_ID, WORKSPACES_ID, [
+      Query.orderDesc("$createdAt"),
+      Query.contains("$id", workspaceIds),
+    ]);
 
     return c.json({ data: workspaces });
   })
@@ -34,6 +49,13 @@ const app = new Hono()
       name,
       userId: user.$id,
       imageUrl: uploadImageUrl,
+      inviteCode: generateInviteCode(6),
+    });
+
+    await databases.createDocument(DATABASE_ID, MEMBERS_ID, ID.unique(), {
+      userId: user.$id,
+      workspaceId: workspace.$id,
+      role: MemberRole.ADMIN,
     });
 
     return c.json({ data: workspace });
