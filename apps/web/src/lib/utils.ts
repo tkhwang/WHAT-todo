d533915eec7414913a692552ef64d775fce1d234
tkhwang <tkhwang@gmail.com>
2024-11-30 15:13:46 +0900

    ✨ feat(web/utils): add generateInviteCode function for invite code generation

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateInviteCode(length: number) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let code = "";
+
+  for (let i = 0; i < length; i += 1) {
+    code += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return code;
 }
